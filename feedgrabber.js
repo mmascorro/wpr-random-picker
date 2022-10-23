8fb52dda0aebec9a22412d5fd762bae20e075d82
Miguel Mascorro <futuretuesday@gmail.com>
2022-10-22 23:32:40 -0500

    url adjustment

--- feedgrabber.js
+++ feedgrabber.js
@@ -1,9 +1,9 @@
 const FeedParser = require('feedparser');
 const { Readable } = require('stream')
-const http = require('http');
+const http = require('https');
 const fs = require('fs');
 
-const url = new URL('http://rss.acast.com/vicegamingsnewpodcast');
+const url = new URL('https://feeds.acast.com/public/shows/0374f776-c411-4314-95ca-bc5eb1b7b6e0');
 const feedparser = new FeedParser();
 const episodes = [];
 let xml = '';
